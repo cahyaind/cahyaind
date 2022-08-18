e098af2e11f0d7cbe7672126ba66cdb1746285e7
cahyaind <cahyaindsi@gmail.com>
2022-08-18 23:05:04 +0700

    site default

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ $(document).ready(function() {
     // event onClick toggle menu
     $toggleCollapse.click(function() {
         $nav.toggleClass('collapse');
+    })
+
+    // owl carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
     });
 
 });
